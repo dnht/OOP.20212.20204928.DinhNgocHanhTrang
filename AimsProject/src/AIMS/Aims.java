9ac23cfdbb23c0071b32071c78810c466fe77812
dnht <95595731+dnht@users.noreply.github.com>
2022-05-07 11:49:31 +0700

    Add files via upload

---
+++ AimsProject/src/AIMS/Aims.java
@@ -0,0 +1,10 @@
+package AIMS;
+
+public class Aims {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AimsProject/src/AIMS/Cart.java
@@ -0,0 +1,9 @@
+package AIMS;
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemOrdered[] = 
+			new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	
+	
+}
