9d44b98f92e56df0c3a2782d31399e25b6f0728b
dnht <95595731+dnht@users.noreply.github.com>
2022-06-18 11:45:12 +0700

    lab07

---
+++ GUIProject/src/hust/soict/dsai/swing/SwingAccumulator.java
@@ -0,0 +1,44 @@
+package hust.soict.dsai.swing;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SwingAccumulator {
+	private JTextField tfInput;
+	private JTextField tfOutput;
+	private int sum = 0;
+	
+	// setup GUI 
+	public SwingAccumulator() {
+		Container cp = getContentPane();
+		cp.setLayout(new GridLayout(2,2));
+		
+		cp.add(new JLabel("Enter an Integer: "));
+		tfInput = new JTextField(10);
+		
+		cp.add(new JLabel("The Accumulated Sum is: "));
+		
+		tfOutput = new JTextField(10);
+		tfOutput.setEditable(false);
+		cp.add(tfOutput);
+		
+		setTitle("Swing Accumulator: ");
+		setSize(350, 120);
+		setVisible(true);
+	}
+
+	public static void main (String[] args ) {
+		new SwingAccumulator();
+	}
+	
+	private class TFInputListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent evt) {
+			int numberIn = Integer.parseInt(tfInput.getText());
+			sum += numberIn;
+			tfInput.setText("");
+			tfOutput.setText(sum + "");
+		}
+	}
+}
